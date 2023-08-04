0e0ecef465251fb2c8a4bacfae5da24d1016731f
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-08-04 22:37:51 +0200

    add comments

--- contacts.js
+++ contacts.js
@@ -4,11 +4,13 @@ const { nanoid } = require("nanoid");
 
 const contactsPath = path.join(__dirname, "contacts.json");
 
+// Returns an array of contacts.
 async function listContacts() {
   const data = await fs.readFile(contactsPath);
   return JSON.parse(data);
 }
 
+// Returns the contact object with this id. Returns null if no contact with this id is found.
 async function getContactById(contactId) {
   const id = String(contactId);
   const contacts = await listContacts();
@@ -16,18 +18,7 @@ async function getContactById(contactId) {
   return result || null;
 }
 
-async function removeContact(contactId) {
-  const id = String(contactId);
-  const contacts = await listContacts();
-  const index = contacts.findIndex((item) => item.id === id);
-  if (index === -1) {
-    return null;
-  }
-  const [result] = contacts.splice(index, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return result;
-}
-
+// Returns the object of the added contact.
 async function addContact(name, email, phone) {
   const contacts = await listContacts();
   const newContact = {
@@ -41,9 +32,22 @@ async function addContact(name, email, phone) {
   return newContact;
 }
 
+// Returns the deleted contact object. Returns null if no contact with this id is found.
+async function removeContact(contactId) {
+  const id = String(contactId);
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
+
 module.exports = {
   listContacts,
   getContactById,
-  removeContact,
   addContact,
+  removeContact,
 };
