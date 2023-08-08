cec9106dc5b1356b0fd599b186b4ac58af9a14f7
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-08-08 19:36:27 +0200

    fix json location

--- contacts.js
+++ contacts.js
@@ -2,7 +2,7 @@ const fs = require("node:fs/promises");
 const path = require("node:path");
 const { nanoid } = require("nanoid");
 
-const contactsPath = path.join(__dirname, "contacts.json");
+const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 // Returns an array of contacts.
 async function listContacts() {
